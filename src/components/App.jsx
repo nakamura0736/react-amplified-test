acf143c079141920948d5df362fb89f78d5661ac
nakamura0736 <tel07360102@gmail.com>
2020-11-24 22:42:48 +0900

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <header className="pv5 bg-gold black-80">
+        <h1 className="mt0 mb1 tc">Hello Amplify! I'm Nakamura</h1>
+        <div className="tc ttc">(・∀・) How are you ? </div>
+        <div className="tc ttc">I’m so amplified !! (・∀・)</div>
+      </header>
+      <div className="pt4 pb1 tc">Go save the world with JavaScript</div>
+      <div className="tc">and edit <code>src/components/<span className="b">App.jsx</span></code>!</div>
+    </>
+  )
+}
+
+export default App
